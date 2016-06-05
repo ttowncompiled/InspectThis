d795390b58a31adbb993272a29d49258de937ee2
Ian Riley <ian.riley.915@gmail.com>
2016-06-04 22:03:18 -0500

    Renters form can handle gestures with animations.

--- app/pages/renters-walkthrough-inspection-form/renters-walkthrough-inspection-form.ts
+++ app/pages/renters-walkthrough-inspection-form/renters-walkthrough-inspection-form.ts
@@ -1,4 +1,5 @@
 import {Page, NavController} from 'ionic-angular';
+import {ComponentRef} from '@angular/core';
 import {Check, Database} from '../../providers/database/database';
 import {JQuery} from '../../providers/j-query/j-query';
 
@@ -14,12 +15,59 @@ import {JQuery} from '../../providers/j-query/j-query';
 })
 export class RentersWalkthroughInspectionFormPage {
   checks: Check[] = [];
+  activeAnimation: string = '';
+  activeCheck: Check = null;
+  activeIndex: number = 0;
   
   constructor(public nav: NavController, public db: Database, public jq: JQuery) {
     this.loadChecks();
   }
   
+  onAnimationEnd(): void {
+    this.loadNextCheck();
+    this.jq.$('#card').removeClass('animated ' + this.activeAnimation);
+    this.activeAnimation = '';
+  }
+  
   loadChecks(): void {
-    this.db.loadChecks('/').then((checks: Check[]) => this.checks = checks);
+    this.db.loadChecks('/').then((checks: Check[]) => {
+      this.checks = checks;
+      this.activeCheck = this.checks[this.activeIndex];
+    });
+  }
+  
+  loadNextCheck(): void {
+    if (this.activeIndex == this.checks.length) {
+      this.activeCheck = null;
+      return;
+    }
+    this.activeIndex++;
+    this.activeCheck = this.checks[this.activeIndex];
+  }
+  
+  onClick(): void {
+    this.activeCheck.value = 'na';
+    this.activeAnimation = JQuery.ROTATE_OUT;
+    this.jq.animate('card', this.activeAnimation).one(JQuery.ANIMATION_END, () => this.onAnimationEnd());
+  }
+  
+  onSwipe(swipe: any): void {
+    if (swipe.direction == JQuery.SWIPE_LEFT) {
+      this.swipeLeft();
+    } else if (swipe.direction == JQuery.SWIPE_RIGHT) {
+      this.swipeRight();
+    }
+  }
+  
+  swipeLeft(): void {
+    this.activeCheck.value = 'no';
+    this.activeAnimation = JQuery.SLIDE_OUT_LEFT;
+    this.jq.animate('card', this.activeAnimation).one(JQuery.ANIMATION_END, () => this.onAnimationEnd());
+  }
+  
+  swipeRight(): void {
+    this.activeCheck.value = 'yes';
+    this.activeAnimation = JQuery.SLIDE_OUT_RIGHT;
+    this.jq.animate('card', this.activeAnimation).one(JQuery.ANIMATION_END, () => this.onAnimationEnd());
   }
 }
--- app/providers/j-query/j-query.ts
+++ app/providers/j-query/j-query.ts
@@ -10,10 +10,21 @@ declare var jQuery;
 */
 @Injectable()
 export class JQuery {
+  static ANIMATION_END: string = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+  static ROTATE_OUT: string = 'rotateOut';
+  static SLIDE_OUT_LEFT: string = 'slideOutLeft';
+  static SLIDE_OUT_RIGHT: string = 'slideOutRight';
+  static SWIPE_LEFT: number = 2;
+  static SWIPE_RIGHT: number = 4;
+  
   $: any = null;
 
   constructor() {
     this.$ = jQuery;
+  }
+  
+  animate(id: string, animation: string): any {
+    return this.$('#' + id).addClass('animated ' + animation);
   }
 
 }
